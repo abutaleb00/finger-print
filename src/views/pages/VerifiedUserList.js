a394136a91a1a9c2e2763eb2cf004e07b8cd0a51
Abu Taleb <abutaleb142@gmail.com>
2023-08-20 09:08:32 +0600

    dependency update

--- src/views/pages/VerifiedUserList.js
+++ src/views/pages/VerifiedUserList.js
@@ -14,7 +14,7 @@ import { Link } from "react-router-dom";
 import "flatpickr/dist/themes/airbnb.css";
 // ** Third Party Components
 import "cleave.js/dist/addons/cleave-phone.us";
-import MUIDataTable from "mui-datatables";
+// import MUIDataTable from "mui-datatables";
 // ** Utils
 
 // ** Styles
@@ -23,23 +23,23 @@ import "@styles/react/libs/react-select/_react-select.scss";
 
 const VerifiedUserList = () => {
 
-  const columns = ["Name", "Father Name", "Date of Birth", "Mobile No", "Gender", "Profession", "Relation"];
+  // const columns = ["Name", "Father Name", "Date of Birth", "Mobile No", "Gender", "Profession", "Relation"];
 
-  const data = [
-    ["Abdul Aziz", "Md. Samaz", "16-02-1995", "01926543252", "Male", "Business", "Friend"],
-    ["Ataur rahman", "Md. Kasem Shikdar", "17-05-1993", "0192353556", "Make", "Service Holder", "Brother"]
-  ];
+  // const data = [
+  //   ["Abdul Aziz", "Md. Samaz", "16-02-1995", "01926543252", "Male", "Business", "Friend"],
+  //   ["Ataur rahman", "Md. Kasem Shikdar", "17-05-1993", "0192353556", "Make", "Service Holder", "Brother"]
+  // ];
 
-  const options = {
-    filterType: "checkbox",
-    responsive: "standard",
-    filter: false,
-    download: false,
-    print: false,
-    selectableRows: "none",
-    rowsPerPage: 100,
-    rowsPerPageOptions: [10, 100, 200, 500, 1000],
-  };
+  // const options = {
+  //   filterType: "checkbox",
+  //   responsive: "standard",
+  //   filter: false,
+  //   download: false,
+  //   print: false,
+  //   selectableRows: "none",
+  //   rowsPerPage: 100,
+  //   rowsPerPageOptions: [10, 100, 200, 500, 1000],
+  // };
 
   return (
     <Card>
@@ -54,12 +54,12 @@ const VerifiedUserList = () => {
         </Link>
       </Col>
       </Row>
-        <MUIDataTable
+        {/* <MUIDataTable
           title={"Grantors List"}
           data={data}
           columns={columns}
           options={options}
-        />
+        /> */}
       </CardBody>
     </Card>
   );
